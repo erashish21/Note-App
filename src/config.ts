e0bc50d6dad5df92e5ab99dbb4eaebb9c9690736
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2024-09-12 18:38:17 +0530

    db update

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,12 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectionLimit: 20,
-  connectTimeout: 30000
+  connectionLimit: 10,  
+  acquireTimeout: 20000, 
+  connectTimeout: 30000, 
+  idleTimeout: 10000,   
 });
+
 
 
 async function testConnection() {
