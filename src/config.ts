400cb2e4e0a9e7a1abb13bc52b1c4a6cce4a5e64
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2024-09-12 17:51:33 +0530

    port update

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,6 @@ const pool = mariadb.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: 3305,  
   connectionLimit: 20,
   connectTimeout: 30000
 });
