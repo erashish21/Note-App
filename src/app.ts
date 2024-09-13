c9bee3f54768e7c9df45f0718e4e01a51ce9f198
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2024-09-13 17:54:50 +0530

    cors add

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import dotenv from 'dotenv';
 import noteRoutes from './routes/noteRoutes';
-
+import cors from 'cors';
 const app = express();
 dotenv.config();
 
@@ -12,9 +12,14 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.json());
 app.use('/api', noteRoutes);
 
+const corsOptions = {
+   origin: ['http://localhost:3000', 'https://note-app-89bt.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // If you need to send cookies or auth headers
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 3000;
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- src/routes/noteRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const noteController_1 = require("../controllers/noteController");
-const router = (0, express_1.Router)();
-router.post('/notes', noteController_1.NoteController.save);
-router.get('/notes', noteController_1.NoteController.list);
-router.delete('/notes/:id', noteController_1.NoteController.delete);
-exports.default = router;
